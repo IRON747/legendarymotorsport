6234f67e990daaad59699c88b637602ee66c061e
Durgesh Kumar <71866455+IRON747@users.noreply.github.com>
2021-11-24 01:51:51 +0530

    debug true

--- legendarymotorsport/settings.py
+++ legendarymotorsport/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h-t#)+2^0-w2vs-pzpqx)_yutsp=k%k($ndq1)tx&g+gkv+*rr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['legendarymotorsport.herokuapp.com']
 
